bf3e095697a688ff83f9d183cb688ca7d71437a3
Ivan Matijas <ivan.vitz.matijas@gmail.com>
2024-06-14 10:27:40 +0100

    Added user details modal with basic styling

--- src/app/components/UserDetails/index.tsx
+++ src/app/components/UserDetails/index.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { ReactNode, useEffect } from 'react';
+import styles from "./index.module.css";
+import { UserInformationProps } from '@/consts/interfaces';
 
-const UserDetails: React.FC<any> = () => {
-  return <></>
+interface UserDetailsProps {
+  isOpen: boolean;
+  onClose: () => void;
+  data: UserInformationProps;
 }
+
+const UserDetails: React.FC<UserDetailsProps> = ({ isOpen, onClose, data }) => {
+  useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+    document.addEventListener('keydown', handleEsc);
+    return () => {
+      document.removeEventListener('keydown', handleEsc);
+    };
+  }, [onClose]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className={styles.userDetailsModalOverlay} onClick={onClose}>
+      <div className={styles.userDetailsModalContent} onClick={e => e.stopPropagation()}>
+        <h2>{data.name}</h2>
+
+        <h3>Contact details</h3>
+        <p>Email: {data.email}</p>
+        <p>Phone: {data.phone}</p>
+
+        <h3>Web</h3>
+        <p>Username: {data.username}</p>
+        <p>Website: {data.website}</p>
+
+        <h3>Company</h3>
+        <p>{data.company.name}</p>
+        <p>Summary: {data.company.bs}</p>
+        <p>{data.company.catchPhrase}</p>
+
+        <h3>Address</h3>
+        <p>{`${data.address.suite} ${data.address.street}, ${data.address.city}, ${data.address.zipcode}`}</p>
+        <p>{`${data.address.geo.lat}, ${data.address.geo.lng}`}</p>
+        <button className={styles.userDetailsModalCloseButton} onClick={onClose}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default UserDetails;
--- src/app/components/UserList/index.tsx
+++ src/app/components/UserList/index.tsx
@@ -1,13 +1,26 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
 import useFetchUsers from "@/app/hooks/useFetchUsers";
 import { UserInformationProps } from "@/consts/interfaces";
 import LoadingSpinner from "../LoadingSpinner";
+import UserDetails from "../UserDetails";
 
-const UserList: React.FC<any> = () => {
+const UserList: React.FC = () => {
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+  const [userDetails, setUserDetails] = useState<UserInformationProps | null>(null)
   const { users, error, loading } = useFetchUsers();
+
+
+  const handleOpenModal = (user: UserInformationProps) => {
+    setUserDetails(user);
+    setModalOpen(true);
+  }
+  const handleCloseModal = () => {
+    setUserDetails(null);
+    setModalOpen(false)
+  };
 
   return (
     <div className={styles.userListDisplayContainer}>
@@ -20,7 +33,7 @@ const UserList: React.FC<any> = () => {
           </div>
           {users?.map((user: UserInformationProps, index: number) => {
             return (
-              <div className={styles.userDisplayBasicInfo} key={`user-management-list-${index}-${user.id}`}>
+              <div className={styles.userDisplayBasicInfo} key={`user-management-list-${index}-${user.id}`} onClick={() => handleOpenModal(user)}>
                 <p className={styles.userDisplayItem}>{user.name}</p>
                 <p className={styles.userDisplayItem}>{user.email}</p>
                 <p className={styles.userDisplayItem}>{user.phone}</p>
@@ -31,6 +44,8 @@ const UserList: React.FC<any> = () => {
       )}
       {loading && <div className={styles.userListDisplayLoadingContainer}><LoadingSpinner /></div>}
       {error && <p className={styles.userListErrorMessage}>{error}</p>}
+
+      {userDetails && <UserDetails isOpen={isModalOpen} onClose={handleCloseModal} data={userDetails}/>}
     </div>
   );
 };
