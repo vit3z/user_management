e707ca89091d3286f04bc06bec7f21b172e3a3d5
Ivan Matijas <ivan.vitz.matijas@gmail.com>
2024-06-14 11:32:09 +0100

    Modal grouping and additional styling

--- src/app/components/UserDetails/index.tsx
+++ src/app/components/UserDetails/index.tsx
@@ -1,6 +1,8 @@
-import React, { ReactNode, useEffect } from 'react';
+import React, { ReactNode, useEffect } from "react";
 import styles from "./index.module.css";
-import { UserInformationProps } from '@/consts/interfaces';
+import { UserInformationProps } from "@/consts/interfaces";
+import Image from "next/image";
+import ProfileImage from "../../../../public/Profile_avatar_placeholder_large.png";
 
 interface UserDetailsProps {
   isOpen: boolean;
@@ -11,13 +13,13 @@ interface UserDetailsProps {
 const UserDetails: React.FC<UserDetailsProps> = ({ isOpen, onClose, data }) => {
   useEffect(() => {
     const handleEsc = (event: KeyboardEvent) => {
-      if (event.key === 'Escape') {
+      if (event.key === "Escape") {
         onClose();
       }
     };
-    document.addEventListener('keydown', handleEsc);
+    document.addEventListener("keydown", handleEsc);
     return () => {
-      document.removeEventListener('keydown', handleEsc);
+      document.removeEventListener("keydown", handleEsc);
     };
   }, [onClose]);
 
@@ -27,28 +29,81 @@ const UserDetails: React.FC<UserDetailsProps> = ({ isOpen, onClose, data }) => {
 
   return (
     <div className={styles.userDetailsModalOverlay} onClick={onClose}>
-      <div className={styles.userDetailsModalContent} onClick={e => e.stopPropagation()}>
-        <h2>{data.name}</h2>
+      <div
+        className={styles.userDetailsModalContent}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className={styles.userDetailsModalHeaderSection}>
+          <Image
+            className={styles.userDetailsModalUserAvatar}
+            src={ProfileImage}
+            width={50}
+            height={50}
+            alt="User Avatar"
+          />
+          <h2 className={styles.userDetailsModalContentUserName}>
+            {data.name}
+          </h2>
+          <a
+            href={`https://google.com `}
+            target="_blank"
+            className={styles.userDetailsModalContentUserWebsite}
+          >
+            {data.website}
+          </a>
+          <hr className={styles.userDetailsModalDivider} />
+        </div>
 
-        <h3>Contact details</h3>
-        <p>Email: {data.email}</p>
-        <p>Phone: {data.phone}</p>
+        <div className={styles.userDetailsModalBodySection}>
+          <h3 className={styles.userDetailsModalContentSectionTitle}>
+            Contact details
+          </h3>
+          <p className={styles.userDetailsModalContentSectionInfo}>
+            <a href={`mailto:${data.email}`}>
+              Email: <b>{data.email}</b>{" "}
+            </a>
+          </p>
+          <p className={styles.userDetailsModalContentSectionInfo}>
+            <a href={`tel:${data.phone}`}>
+              Phone: <b>{data.phone}</b>
+            </a>
+          </p>
+          <p className={styles.userDetailsModalContentSectionInfo}>
+            Username: <b>{data.username}</b>
+          </p>
 
-        <h3>Web</h3>
-        <p>Username: {data.username}</p>
-        <p>Website: {data.website}</p>
+          <h3 className={styles.userDetailsModalContentSectionTitle}>
+            Company
+          </h3>
+          <p className={styles.userDetailsModalContentSectionInfo}>
+            <b>{data.company.name}</b>
+          </p>
+          <p className={styles.userDetailsModalContentSectionInfo}>
+            Summary: <b>{data.company.bs}</b>
+          </p>
+          <p className={styles.userDetailsModalContentSectionInfo}>
+            <b>{data.company.catchPhrase}</b>
+          </p>
 
-        <h3>Company</h3>
-        <p>{data.company.name}</p>
-        <p>Summary: {data.company.bs}</p>
-        <p>{data.company.catchPhrase}</p>
-
-        <h3>Address</h3>
-        <p>{`${data.address.suite} ${data.address.street}, ${data.address.city}, ${data.address.zipcode}`}</p>
-        <p>{`${data.address.geo.lat}, ${data.address.geo.lng}`}</p>
-        <button className={styles.userDetailsModalCloseButton} onClick={onClose}>
-          Close
-        </button>
+          <h3 className={styles.userDetailsModalContentSectionTitle}>
+            Address
+          </h3>
+          <p className={styles.userDetailsModalContentSectionInfo}>
+            <b>{`${data.address.suite} ${data.address.street}, ${data.address.city}`}</b>
+          </p>
+          <p className={styles.userDetailsModalContentSectionInfo}>
+            <b>{data.address.zipcode}</b>
+          </p>
+          <p
+            className={styles.userDetailsModalContentSectionInfoSmallText}
+          >{`${data.address.geo.lat}, ${data.address.geo.lng}`}</p>
+          <button
+            className={styles.userDetailsModalCloseButton}
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
       </div>
     </div>
   );
